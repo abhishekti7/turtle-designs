0901bc047584e6ae694263cd64585f0b05257723
Abhishek Tiwari <abhishek.tiwari3507@gmail.com>
2018-02-17 14:44:56 +0530

    Add files via upload

---
+++ turtle3.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+pencolor('white')
+bgcolor('black')
+speed(400)
+size = 36
+x = 0
+
+while x<size:
+	rt(90)
+	circle(60)
+	lt(120)
+	circle(90)
+	lt(120)
+	circle(120)
+	circle(150)
+
+	x = x+1
+
+ht()
